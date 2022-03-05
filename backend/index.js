22574e9233cc3a75dc5eefefdc1dcb446a67b20c
Udit <royalboy0012@gmail.com>
2022-03-05 13:21:00 +0530

    make it better

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mongoConnect = require("./db");
 const app = express();
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config()
-const port = process.env.NODE_PORT ||  8000;
+const port = process.env.PORT || 8000;
 
 mongoConnect();
 app.use(cors())
@@ -11,6 +12,18 @@ app.use(express.json());
 
 app.use('/inotesApi/auth',require('./routes/auth'));
 app.use('/inotesApi/notes',require('./routes/notes'));
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,"/frontend/build")));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    })
+}else{
+    app.get("/",(req,res)=>{
+        res.send("APP running successfully");
+    })
+}
 
 app.listen(port,()=>{
     console.log(`app is listening at http://localhost:${port}`);
