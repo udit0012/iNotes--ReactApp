194783b57df57640b7341251be4d522b9d534f3e
Udit <royalboy0012@gmail.com>
2022-03-05 16:15:50 +0530

    Screenshots added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <Router>
       <NoteState>
-        <div className="w-full h-[92%] md:h-screen">
+        <div className="w-full h-[96vh] md:h-screen">
           <Alert />
           <Routes>
             <Route path="/" exact element={<Homepage />} />
--- frontend/src/components/Authentictaion/Auth.js
+++ frontend/src/components/Authentictaion/Auth.js
@@ -6,7 +6,7 @@ import Register from './Register';
 const Auth = () => {
     const [toggle, setToggle] = useState(false);
     return (
-        <div className="md:flex w-screen h-screen relative">
+        <div className="md:flex w-full h-full relative">
             <div className='lg:w-1/6 hidden lg:block'><Sidebar /></div>
             <div className='w-full h-full lg:w-5/6 bg-slate-100 flex flex-col items-center justify-start py-16 md:py-24'>
                 <div className="lg:hidden text-2xl md:text-4xl px-3 py-2 md:px-6 md:py-4 text-green-700 font-serif">
