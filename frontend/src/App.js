6113623b041f54599ca82a69a1eb9513ed2d5a84
Udit <royalboy0012@gmail.com>
2022-03-05 16:09:28 +0530

    Screenshots added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <Router>
       <NoteState>
-        <div className="w-full h-full">
+        <div className="w-full">
           <Alert />
           <Routes>
             <Route path="/" exact element={<Homepage />} />
